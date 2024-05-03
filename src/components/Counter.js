290f0152205822cfb75fe2fb826ce691354b2162
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-05-03 22:44:13 +0545

    handling multiple slices with redux and adding payload

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,25 +4,35 @@ import { useSelector, useDispatch } from 'react-redux';
 const Counter = () => {
   const dispatch= useDispatch();
 
-  const counter= useSelector(state=>state.count);
+  const counter= useSelector(state=>state.counter);
+
+  const show= useSelector(state=>state.showCounter);
 
   
   const incrementHandler= ()=> {
     dispatch({ type: 'increment' });
   };
 
+  const increase= ()=> {
+    dispatch({ type: 'increase', amount: 5});
+  };
+
   const decrementHandler= ()=> {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
     <div>
       <button onClick={incrementHandler}>Increment</button>
+      <button onClick={increase}>Incrementby5</button>
+      
       <button onClick={decrementHandler} >Decrement</button>
     </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,32 @@
 
 import { createStore} from 'redux';
 
-const initialState= {count: 0};
+const initialState= {counter: 0, showCounter: true};
 
 const counterReducer= (state= initialState, action)=> {
 
     if (action.type === 'increment') {
         return {
-            count: state.count + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
+        };
+    }
+    if(action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
         };
     }
     if (action.type === 'decrement') {
         return {
-            count: state.count -1
+            counter: state.counter -1,
+            showCounter: state.showCounter
+        };
+    }
+    if (action.type === 'toggle') {
+        return{
+            counter: state.counter,
+            showCounter: !state.showCounter
         };
     }
     return state;
